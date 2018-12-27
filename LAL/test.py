4d9005498c5881722ccb2aa7dee2785f12e5029f
BlackroseLi <3123652@qq.com>
2018-12-27 15:44:39 +0800

    chan

---
+++ LAL/test.py
@@ -0,0 +1,14 @@
+import os
+os.chdir(r'C:\Users\31236\Desktop\baseline\LAL')
+
+import numpy as np 
+
+fn = 'LAL-randomtree-simulatedunbalanced-big.npz'
+# we found these parameters by cross-validating the regressor and now we reuse these expreiments
+parameters = {'est': 2000, 'depth': 40, 'feat': 6 }
+filename = './lal datasets/'+fn
+regression_data = np.load(filename)
+regression_features = regression_data['arr_0']
+regression_labels = regression_data['arr_1']
+
+print(np.shape(regression_features))
---
+++ base.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class BaseLine():
+    """
+    """
+    def __init__(self, model):
+        pass
+    
+    def 
