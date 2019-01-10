c7cfd1529c45f4961dabcf75d824f5a9e9c8e401
BlackroseLi <3123652@qq.com>
2019-01-10 12:15:39 +0800

    data

--- LAL/baseline/meta_data.py
+++ LAL/baseline/meta_data.py
@@ -1,24 +1,76 @@
+"""
+Meta features designing for binary classification tasks 
+ in the pool based active learning scenario.
+"""
+import os
 import numpy as np 
 from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import Normalizer,minmax_scale
-
+from sklearn.utils.validation import check_array
 from sklearn.datasets import make_classification
 from sklearn.svm import SVC
+
+def randperm(n, k=None):
+    """Generate a random array which contains k elements range from (n[0]:n[1])
+
+    Parameters
+    ----------
+    n: int or tuple
+        range from [n[0]:n[1]], include n[0] and n[1].
+        if an int is given, then n[0] = 0
+
+    k: int, optional (default=end - start + 1)
+        how many numbers will be generated. should not larger than n[1]-n[0]+1,
+        default=n[1] - n[0] + 1.
+
+    Returns
+    -------
+    perm: list
+        the generated array.
+    """
+    if isinstance(n, np.generic):
+        n = np.asscalar(n)
+    if isinstance(n, tuple):
+        if n[0] is not None:
+            start = n[0]
+        else:
+            start = 0
+        end = n[1]
+    elif isinstance(n, int):
+        start = 0
+        end = n
+    else:
+        raise TypeError("n must be tuple or int.")
+
+    if k is None:
+        k = end - start + 1
+    if not isinstance(k, int):
+        raise TypeError("k must be an int.")
+    if k > end - start + 1:
+        raise ValueError("k should not larger than n[1]-n[0]+1")
+
+    randarr = np.arange(start, end + 1)
+    np.random.shuffle(randarr)
+    return randarr[0:k]
 
 class DataSet():
     """
 
     Parameters
     ----------
-    X: 2D
-        The dataset.
-    
+    X: 2D array, optional (default=None) [n_samples, n_features]
+        Feature matrix of the whole dataset. It is a reference which will not use additional memory.
+
+    y: array-like, optional (default=None) [n_samples]
+        Label matrix of the whole dataset. It is a reference which will not use additional memory.
+        
     """
-    def __init__(self, X):
+    def __init__(self, X, y):
         if not isinstance(X, (list, np.ndarray)):
             raise ValueError("")
         self.X = X
+        self.y = y
 
         self.n_samples, self.n_features =  np.shape(X)
         self.distance = None
@@ -58,7 +110,6 @@ class DataSet():
 
         return X[index_cluster_centers], index_cluster_centers
 
-
     def get_distance(self, method='Euclidean'):
         """
 
@@ -82,9 +133,141 @@ class DataSet():
         
         self.distance = distance + distance.T
         return self.distance
+    
+    def split_data(self, test_ratio=0.3, initial_label_rate=0.05, split_count=10, saving_path='.'):
+        """Split given data.
+
+        Parameters
+        ----------
+        test_ratio: float, optional (default=0.3)
+            Ratio of test set
+
+        initial_label_rate: float, optional (default=0.05)
+            Ratio of initial label set
+            e.g. Initial_labelset*(1-test_ratio)*n_samples
+
+        split_count: int, optional (default=10)
+            Random split data _split_count times
+
+        saving_path: str, optional (default='.')
+            Giving None to disable saving.
+
+        Returns
+        -------
+        train_idx: list
+            index of training set, shape like [n_split_count, n_training_indexes]
+
+        test_idx: list
+            index of testing set, shape like [n_split_count, n_testing_indexes]
+
+        label_idx: list
+            index of labeling set, shape like [n_split_count, n_labeling_indexes]
+
+        unlabel_idx: list
+            index of unlabeling set, shape like [n_split_count, n_unlabeling_indexes]
+        """
+        # check parameters
+        len_of_parameters = [len(self.X) if X is not None else None, len(self.y) if y is not None else None]
+        number_of_instance = np.unique([i for i in len_of_parameters if i is not None])
+        if len(number_of_instance) > 1:
+            raise ValueError("Different length of instances and _labels found.")
+        else:
+            number_of_instance = number_of_instance[0]
+
+        instance_indexes = np.arange(number_of_instance)
+
+        # split
+        train_idx = []
+        test_idx = []
+        label_idx = []
+        unlabel_idx = []
+        for i in range(split_count):
+            rp = randperm(number_of_instance - 1)
+            cutpoint = round((1 - test_ratio) * len(rp))
+            tp_train = instance_indexes[rp[0:cutpoint]]
+            train_idx.append(tp_train)
+            test_idx.append(instance_indexes[rp[cutpoint:]])
+            cutpoint = round(initial_label_rate * len(tp_train))
+            if cutpoint <= 1:
+                cutpoint = 1
+            label_idx.append(tp_train[0:cutpoint])
+            unlabel_idx.append(tp_train[cutpoint:])
+
+        self.split_save(train_idx=train_idx, test_idx=test_idx, label_idx=label_idx,
+                unlabel_idx=unlabel_idx, path=saving_path)
+        return train_idx, test_idx, label_idx, unlabel_idx
+
+    def split_load(self, path):
+        """Load split from path.
+
+        Parameters
+        ----------
+        path: str
+            Path to a dir which contains train_idx.txt, test_idx.txt, label_idx.txt, unlabel_idx.txt.
+
+        Returns
+        -------
+        train_idx: list
+            index of training set, shape like [n_split_count, n_training_samples]
+
+        test_idx: list
+            index of testing set, shape like [n_split_count, n_testing_samples]
+
+        label_idx: list
+            index of labeling set, shape like [n_split_count, n_labeling_samples]
+
+        unlabel_idx: list
+            index of unlabeling set, shape like [n_split_count, n_unlabeling_samples]
+        """
+        if not isinstance(path, str):
+            raise TypeError("A string is expected, but received: %s" % str(type(path)))
+        saving_path = os.path.abspath(path)
+        if not os.path.isdir(saving_path):
+            raise Exception("A path to a directory is expected.")
+
+        ret_arr = []
+        for fname in ['train_idx.txt', 'test_idx.txt', 'label_idx.txt', 'unlabel_idx.txt']:
+            if not os.path.exists(os.path.join(saving_path, fname)):
+                if os.path.exists(os.path.join(saving_path, fname.split()[0] + '.npy')):
+                    ret_arr.append(np.load(os.path.join(saving_path, fname.split()[0] + '.npy')))
+                else:
+                    ret_arr.append(None)
+            else:
+                ret_arr.append(np.loadtxt(os.path.join(saving_path, fname)))
+        return ret_arr[0], ret_arr[1], ret_arr[2], ret_arr[3]
 
 
-def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query_index):
+    def split_save(self, train_idx, test_idx, label_idx, unlabel_idx, path):
+        """Save the split to file for auditting or loading for other methods.
+
+        Parameters
+        ----------
+        saving_path: str
+            path to save the settings. If a dir is not provided, it will generate a folder called
+            'alipy_split' for saving.
+
+        """
+        if path is None:
+            return
+        else:
+            if not isinstance(path, str):
+                raise TypeError("A string is expected, but received: %s" % str(type(path)))
+
+        saving_path = os.path.abspath(path)
+        if os.path.isdir(saving_path):
+            np.savetxt(os.path.join(saving_path, 'train_idx.txt'), train_idx)
+            np.savetxt(os.path.join(saving_path, 'test_idx.txt'), test_idx)
+            if len(np.shape(label_idx)) == 2:
+                np.savetxt(os.path.join(saving_path, 'label_idx.txt'), label_idx)
+                np.savetxt(os.path.join(saving_path, 'unlabel_idx.txt'), unlabel_idx)
+            else:
+                np.save(os.path.join(saving_path, 'label_idx.npy'), label_idx)
+                np.save(os.path.join(saving_path, 'unlabel_idx.npy'), unlabel_idx)
+        else:
+            raise Exception("A path to a directory is expected.")
+
+
+def mate_data(X, y, label_indexs, unlabel_indexs, modelPredictions, query_index):
     """Calculate the meta data according to the current model,dataset and five rounds before information.
 
 
@@ -93,7 +276,7 @@ def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query
     X: 2D array
         Feature matrix of the whole dataset. It is a reference which will not use additional memory.
 
-    label_ys:  {list, np.ndarray}
+    y:  {list, np.ndarray}
         The true label of the each round of iteration,corresponding to label_indexs.
 
     label_indexs: {list, np.ndarray} shape=(number_iteration, corresponding_label_index)
@@ -115,7 +298,6 @@ def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query
     """
 
     for i in range(6):
-        assert(len(label_ys[i]) == len(label_indexs[i]))
         assert(np.shape(X)[0] == np.shape(modelPredictions[i])[0]) 
         if(not isinstance(label_indexs[i], np.ndarray)):
             label_indexs[i] = np.array(label_indexs[i])
@@ -126,7 +308,7 @@ def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query
     query_index_size = len(query_index)
     n_feature_data = n_feature * np.ones((query_index_size, 1))
     current_label_size = len(label_indexs[5])
-    current_label_y = label_ys[5]
+    current_label_y = y[label_indexs[5]]
     current_unlabel_size = len(unlabel_indexs[5])
     current_prediction = modelPredictions[5]
 
@@ -215,7 +397,7 @@ def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query
         label_ind = label_indexs[i]
         unlabel_ind = unlabel_indexs[i]
 
-        tn, fp, fn, tp = confusion_matrix(label_ys[i], cur_prediction[label_ind], labels=[-1, 1]).ravel()
+        tn, fp, fn, tp = confusion_matrix(y[label_ind], cur_prediction[label_ind], labels=[-1, 1]).ravel()
         ratio_tn.append(tn / label_size)
         ratio_fp.append(fp / label_size)
         ratio_fn.append(fn / label_size)
@@ -270,31 +452,32 @@ def mate_data(X, label_ys, label_indexs, unlabel_indexs, modelPredictions, query
 if __name__ == "__main__":
     X, y = make_classification(n_samples=100, n_features=5, n_classes=2)
     y[y==0] = -1
-    labels = []
-    for j in range(10,16):
-        labels.append(np.array([i for i in range(j)]))
-    unlabels = []
-    for j in range(10,16):
-        unlabels.append(np.array([i for i in range(j,100)]))
+    d = DataSet(X, y)
+    cd, cdi = d.get_cluster_center()
+
+    train, test, l_ind, u_ind = d.split_data(split_count=6)
+
+    print(np.shape(train))
+    print(np.shape(l_ind))
+
+    print(np.shape(u_ind))
+    print(l_ind[5])
     
     models = []
     decision_value = []
     prediction = []
-    label_ys = []
 
     for i in range(6):
         model = SVC()
-        model.fit(X[labels[i]], y[labels[i]])
+        model.fit(X[l_ind[i]], y[l_ind[i]])
         prediction.append(model.predict(X))
         decision_value.append(model.decision_function(X))
-        label_ys.append(y[labels[i]])
         models.append(model)
     
     query_index = [i for i in range(15, 21)]
     query_index = np.array(query_index)
-    meta = mate_data(X, label_ys, labels, unlabels, prediction, query_index)
-    d = DataSet(X)
-    cd, cdi = d.get_cluster_center()
-    print('cdi', cdi)
-    
+    meta = mate_data(X, y, l_ind, u_ind, prediction, query_index)
 
+
+
+    
